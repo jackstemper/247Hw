2794f2ed79e38608030b17fc2849e3ce63500065
jhill08 <jah18@email.sc.edu>
2020-03-23 19:55:59 -0400

    Update LoadCommand.java

--- CommandDesignPattern/LoadCommand.java
+++ CommandDesignPattern/LoadCommand.java
@@ -1,3 +1,14 @@
 public class LoadCommand implements Command{
 
+    private Document doc;
+
+    public LoadCommand(Document doc){
+        this.doc = doc;
+
+    }
+
+    @Override
+    public void execute(){
+        doc.load();
+    }
 }
