35d9a19be8b9f20dc8767ba5766660f8a953de66
jhill08 <jah18@email.sc.edu>
2020-03-23 19:47:14 -0400

    Update Document.java

--- CommandDesignPattern/Document.java
+++ CommandDesignPattern/Document.java
@@ -1,6 +1,23 @@
 package csce247.assignments.command;
 
-
 public class Document {
+    public Document(String name){
+        System.out.println("Document named " + name + " is created");
+    }
 
+    public void load(){
+        System.out.println("document " + name + " is loaded into view");
+    }
+
+    public void spell(){
+        System.out.prinln("document " + name + " is being checked for spelling errors");
+    }
+
+    public void save(){
+        System.out.println("document " + name + " is being saved...");
+    }
+
+    public void print(){
+        System.out.println("document " + name + " is printing..");
+    }
 }
