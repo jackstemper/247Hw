6924090b885ae93799d77a7cecb9567687cab686
jhill08 <55595871+jhill08@users.noreply.github.com>
2020-03-23 20:33:18 -0400

    Update SpellCommand.java

--- CommandDesignPattern/SpellCommand.java
+++ CommandDesignPattern/SpellCommand.java
@@ -1,3 +1,16 @@
 public class SpellCommand implements Command{
+  
+    private Document doc;
+
+    public LoadCommand(Document doc){
+        this.doc = doc;
+
+    }
+
+    @Override
+    public void execute(){
+        doc.spell();
+    }
+}
 
 }
