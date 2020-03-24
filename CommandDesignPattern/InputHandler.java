c17dc83d48dd365d4f580aae92f74d287b4c02aa
jhill08 <jah18@email.sc.edu>
2020-03-23 21:04:31 -0400

    Create InputHandler.java

---
+++ CommandDesignPattern/InputHandler.java
@@ -0,0 +1,13 @@
+package csce247.assignments.command;
+
+public class InputHandler {
+
+	public InputHandler(Document document){
+		document.key = value;
+	}
+
+	public void inputEntered(String data){
+
+	}
+
+}
