e65b5b7291af9974042f0c68ea48fd5b37386b50
jtscholz <jtscholz@email.sc.edu>
2020-03-24 10:59:36 -0400

    Update PrintCommand.java

--- CommandDesignPattern/PrintCommand.java
+++ CommandDesignPattern/PrintCommand.java
@@ -2,7 +2,7 @@
  * PrintCommand Class : implements Command interface and calls function 
  * necessary to execute PrintCommand
  */
-public class PrintCommand implements Command{
+public class PrintCommand implements Command {
     private Document doc;
     public PrintCommand(Document doc) {
         this.doc = doc;
