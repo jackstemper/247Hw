8697660e36c3e339640bbc9bf206942576d602be
jtscholz <jtscholz@email.sc.edu>
2020-03-24 11:01:28 -0400

    Update SaveCommand.java

--- CommandDesignPattern/SaveCommand.java
+++ CommandDesignPattern/SaveCommand.java
@@ -6,8 +6,7 @@ public class SaveCommand implements Command {
     private Document doc;  
     public SaveCommand(Document doc) {
         this.doc = doc;
-    }
-  
+    } 
     @Override
     public void execute() {
         doc.save();
