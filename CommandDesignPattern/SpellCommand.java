452d6fd5594b50815ecdd751224835f8ba5e520d
jtscholz <59942012+jtscholz@users.noreply.github.com>
2020-03-23 20:21:16 -0400

    Update spellCommand.java

--- CommandDesignPattern/SpellCommand.java
+++ CommandDesignPattern/SpellCommand.java
@@ -1,3 +1,12 @@
 public class SpellCommand implements Command{
+	private Document doc;
 
+	public SpellCommand(Document doc) {
+		this.doc=doc;
+	}
+	
+	@Override
+	public void execute() {
+		doc.spell();
+	}
 }
