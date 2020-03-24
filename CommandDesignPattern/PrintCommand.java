34ab72aeda23d6f49eb8689ec2e2a8a37d208607
jhill08 <jah18@email.sc.edu>
2020-03-23 20:00:11 -0400

    Update PrintCommand.java

--- CommandDesignPattern/PrintCommand.java
+++ CommandDesignPattern/PrintCommand.java
@@ -1,3 +1,15 @@
 public class PrintCommand implements Command{
 
+    private Document doc;
+
+    public LoadCommand(Document doc){
+        this.doc = doc;
+
+    }
+
+    @Override
+    public void execute(){
+        doc.print();
+    }
+}
 }
