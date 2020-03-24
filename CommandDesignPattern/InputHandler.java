1821e2f8acdfe8e6e9e31ca263bbc0b7fae4cfea
jhill08 <55595871+jhill08@users.noreply.github.com>
2020-03-23 21:08:08 -0400

    Update InputHandler.java

--- CommandDesignPattern/InputHandler.java
+++ CommandDesignPattern/InputHandler.java
@@ -3,7 +3,7 @@ package csce247.assignments.command;
 public class InputHandler {
 
 	public InputHandler(Document document){
-		document.key = value;
+		
 	}
 
 	public void inputEntered(String data){
