9acf59fc210f6f1876948ae9299ab541cbe1fe63
jhill08 <jah18@email.sc.edu>
2020-03-24 10:38:30 -0400

    update

--- CommandDesignPattern/Document.java
+++ CommandDesignPattern/Document.java
@@ -2,23 +2,24 @@
 
 public class Document{
     private String name;
+
     public Document(String name){
         System.out.println("Document named " + name + " is created");
     }
 
     public void load(){
-        System.out.println("document " + this.name + " is loaded into view");
+        System.out.println("document " + name + " is loaded into view");
     }
 
     public void spell(){
-        System.out.println("document " + this.name + " is being checked for spelling errors");
+        System.out.println("document " + name + " is being checked for spelling errors");
     }
 
     public void save(){
-        System.out.println("document " + this.name + " is being saved...");
+        System.out.println("document " + name + " is being saved...");
     }
 
     public void print(){
-        System.out.println("document " + this.name + " is printing..");
+        System.out.println("document " + name + " is printing..");
     }
 }
