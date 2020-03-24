d84ca16f0f815e5e3e557164d90590a6b29acd16
jashort8 <59887775+jashort8@users.noreply.github.com>
2020-03-24 11:11:46 -0400

    Update LoadCommand.java

--- CommandDesignPattern/LoadCommand.java
+++ CommandDesignPattern/LoadCommand.java
@@ -3,10 +3,11 @@
  * necessary to execute LoadCommand
  */
 public class LoadCommand implements Command {
-	private Document doc;
+    private Document doc;
     public LoadCommand(Document doc) {
         this.doc = doc;
     }
+    //Override to execute load
     @Override
     public void execute() {
         doc.load();
