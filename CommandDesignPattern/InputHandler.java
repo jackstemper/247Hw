ab1191aaf72407b72e1e907bddd8284336611a9f
jtscholz <jtscholz@email.sc.edu>
2020-03-24 10:21:33 -0400

    Update InputHandler.java

--- CommandDesignPattern/InputHandler.java
+++ CommandDesignPattern/InputHandler.java
@@ -1,20 +1,22 @@
-package csce247.assignments.command;
-
 import java.util.HashMap;
-
 public class InputHandler {
-	private HashMap<String, Command>commands = new HashMap;
-	public InputHandler(Document document){
-		commands.put("load", LoadCommand);
-		commands.put("save", SaveCommand);
-		commands.put("spell", SpellCheckCommand);
+	private HashMap<String,Command> commands;
+	public InputHandler(Document document) {
+		/*
+		 * commands.put("load",LoadCommand);
+		commands.put("save",SaveCommand);
+		commands.put("spell",SpellCheckCommand);
 		commands.put("print", PrintCommand);
-		
+		*/
+		commands = new HashMap<String,Command>();
+		document.load();
+		document.spell();
+		document.save();
+		document.print();
 	}
-
-	public void inputEntered(String data){
-		if(commands.containsKey(data)){
+	public void inputEntered(String data) {
+		if(commands.containsKey(data)) {
 			commands.get(data);
+		}
 	}
-
 }
