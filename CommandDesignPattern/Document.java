5a9f55bb117ade0f8ae710a63f7d68fca1cc2e8c
jtscholz <jtscholz@email.sc.edu>
2020-03-24 10:56:01 -0400

    Adding comments and adjusting format

--- CommandDesignPattern/Document.java
+++ CommandDesignPattern/Document.java
@@ -1,25 +1,28 @@
-
-
-public class Document{
-    private String name;
-
-    public Document(String name){
+/*
+ * Document class: initializes document name, displays different 
+ * messages when you call each method 
+ */
+public class Document {
+	private String name;
+	//Document Constructor : initializes name lets user know document is created
+    public Document(String name) {
+    	this.name=name;
         System.out.println("Document named " + name + " is created");
     }
-
-    public void load(){
+    /*
+     * Each method displays message based on command called
+     */
+    public void load() {
         System.out.println("document " + name + " is loaded into view");
     }
-
-    public void spell(){
+    public void spell() {
         System.out.println("document " + name + " is being checked for spelling errors");
     }
-
-    public void save(){
+    public void save() {
         System.out.println("document " + name + " is being saved...");
     }
-
-    public void print(){
+    public void print() {
         System.out.println("document " + name + " is printing..");
     }
 }
+
